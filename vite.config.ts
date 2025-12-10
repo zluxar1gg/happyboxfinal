45e84c5597d348466a7ca8e552a271edb5d1715e
zluxar1gg <dima.knyazev0@gmail.com>
2025-12-10 16:32:23 +0800

    Update Vite config to define process.env
    Added process.env definition to prevent site crashes.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  define: {
+    // Эта строчка критически важна для предотвращения падения сайта
+    'process.env': {} 
+  }
 });

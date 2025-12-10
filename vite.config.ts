d1c6cc0e1a9095dc3fdc0580731375d929593764
zluxar1gg <dima.knyazev0@gmail.com>
2025-12-10 16:45:38 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  define: {
+    // Эта строка критически важна для предотвращения падения сайта, если где-то используется process.env
+    'process.env': {} 
+  }
 });

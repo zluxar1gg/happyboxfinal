be6850f4de19427fbdaaa4b090018f791c4640f0
zluxar1gg <dima.knyazev0@gmail.com>
2025-12-10 16:17:57 +0800

    Remove unused define property in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env': {}
-  }
+  plugins: [react()]
 });
